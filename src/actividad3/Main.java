f666afa463b0211635a62086b4ce455bf4728643
Gabriel Zafra <gab.zafra@gmail.com>
2022-10-05 17:06:03 +0200

    actividad 3

--- src/actividad3/Main.java
+++ src/actividad3/Main.java
@@ -1,4 +1,4 @@
-package actividad2;
+package actividad3;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
@@ -9,13 +9,18 @@ import java.util.Locale;
 public class Main {
 
   public static void main(String[] args) {
-    dirList("D:\\DAM\\2022-2023\\");
+    dirList("D:\\DAM\\2022-2023\\", "pdf");
   }
 
   private static void dirList(String path) {
+    dirList(path, "");
+  }
+
+  private static void dirList(String path, String extension) {
     File directory = new File(path);
     File[] lista = directory.listFiles();
-    Arrays.stream(lista).forEach(Main::printFileDetail);
+    Arrays.stream(lista).filter(file -> checkExtension(file, extension))
+        .forEach(Main::printFileDetail);
   }
 
   private static void printFileDetail(File file) {
@@ -29,5 +34,15 @@ public class Main {
     System.out.print("     ");
     System.out.print(file.getName());
     System.out.println();
+  }
+
+  private static boolean checkExtension(File file, String extension) {
+    if (file.isDirectory()) {
+      return false;
+    } else if (!extension.equals("")) {
+      return file.getName().endsWith("." + extension);
+    } else {
+      return !file.getName().contains(".");
+    }
   }
 }
